bb1d0fe4ad3c95259729b8a75bd34e2f04c4905d
= <zrsmead@gmail.com>
2017-05-19 20:15:54 -0700

    shelved twitter gem, trying to access twitter api through unirest and starting auth parameterization

--- app/models/book.rb
+++ app/models/book.rb
@@ -132,7 +132,23 @@ class Book < ApplicationRecord
 	# end
 
 	def self.twitter_get_tweets(username)
-		@twitter_client.user_timeline(username)
+		# @twitter_client.user_timeline(username)
+
+		url_endpoint = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+		username_to_grab = '?screen_name=' + username
+		full_query = url_endpoint + username_to_grab
+
+		tweets = Unirest.get(full_query, 
+			auth: {
+				:oauth_consumer_key => ENV['TWITTER_CONSUMER_KEY'], 
+				:oauth_nonce => [*('a'..'z'),*('0'..'9')].shuffle[0,50].join, # just a random string to distinguish calls
+				:oauth_signature => 'go here -> https://dev.twitter.com/oauth/overview/creating-signatures',
+				:oauth_signature_method => 'HMAC-SHA1',
+				:oauth_timestamp => Time.now.to_i, # gives a special time format - google 'Unix Epoch'
+				:oauth_token => ENV['TWITTER_ACCESS_TOKEN'],
+				:oauth_version => '1.0'
+			}
+		).body
 	end
 
 
