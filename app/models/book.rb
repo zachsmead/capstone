99b1efb6048a16bc100499afa0f7b28457f7ec81
= <zrsmead@gmail.com>
2017-05-19 18:49:04 -0700

    restore point

--- app/models/book.rb
+++ app/models/book.rb
@@ -122,14 +122,14 @@ class Book < ApplicationRecord
 		return local_string
 	end
 
-	def self.twitter_config
-		@twitter_client = Twitter::Client.new do |config|
-		  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-		  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-		  config.access_token = ENV['TWITTER_ACCESS_TOKEN]']
-		  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-		end
-	end
+	# def self.twitter_config
+	# 	@twitter_client = Twitter::Client.new do |config|
+	# 	  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+	# 	  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+	# 	  config.access_token = ENV['TWITTER_ACCESS_TOKEN]']
+	# 	  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+	# 	end
+	# end
 
 	def self.twitter_get_tweets(username)
 		@twitter_client.user_timeline(username)
