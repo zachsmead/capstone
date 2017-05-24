289dc1de47faa6597e552120b61a4b11cc8fb91d
= <zrsmead@gmail.com>
2017-05-24 16:22:44 -0700

    debugging- fixed crash when trying to summarize analysis with not enough keywords

--- app/models/nlu.rb
+++ app/models/nlu.rb
@@ -17,19 +17,27 @@ class Nlu # Natural Language Understanding
 
 		if query_results['keywords']
 			stats[:keywords] = query_results['keywords'][0..4]
-		else
+			puts "*" * 100
+			puts 'if was triggered'
+			puts "*" * 100
+		else # if the analysis was not performed, return blank objects
+			puts "*" * 100
+			puts 'else was triggered'
+			puts "*" * 100
 			stats[:keywords] = []
 			stats[:emotions_summary] = emotions_summary
-
 			return stats
 		end
 
 
 		query_results['keywords'].each do | keyword | # loop through all keywords in the result
-			if !emotions_summary['sentiment']					  # add up all the keywords' relevance-weighted sentiment
-				emotions_summary['sentiment'] = keyword['sentiment']['score'] #* keyword['relevance']
-			else
-				emotions_summary['sentiment'] += keyword['sentiment']['score'] #* keyword['relevance']
+			
+			if keyword['sentiment']
+				if !emotions_summary['sentiment']					  # add up all the keywords' relevance-weighted sentiment
+					emotions_summary['sentiment'] = keyword['sentiment']['score'] #* keyword['relevance']
+				else
+					emotions_summary['sentiment'] += keyword['sentiment']['score'] #* keyword['relevance']
+				end
 			end
 
 
@@ -44,17 +52,44 @@ class Nlu # Natural Language Understanding
 			end
 		end # end query_results['keywords'].each loop
 
+		query_results['entities'].each do | entity | # loop through all entities in the result
+			
+			if entity['sentiment']
+				if !emotions_summary['sentiment']					  # add up all the entities' relevance-weighted sentiment
+					emotions_summary['sentiment'] = entity['sentiment']['score'] #* entity['relevance']
+				else
+					emotions_summary['sentiment'] += entity['sentiment']['score'] #* entity['relevance']
+				end
+			end
+
+
+			if entity['emotion'] 													# if the entity has an emotion hash,
+				entity['emotion'].each do | emotion, score | # add those up weighted by relevance
+					if !emotions_summary[emotion]
+						emotions_summary[emotion] = score #* entity['relevance']
+					else
+						emotions_summary[emotion] += score #* entity['relevance']
+					end
+				end
+			end
+		end # end query_results['entities'].each loop
+
 
 		# finally, average all the emotion scores
-		emotions_summary.each do | emotion, score | 
-			emotions_summary[emotion] = (score / query_results['keywords'].length)
+		emotions_summary.each do | emotion, score |
+			emotions_summary[emotion] = (score / (query_results['keywords'].length + query_results['entities'].length))
 		end
 
 		# or express emotion scores as % total of the whole document
 		# start by adding all the emotions up for a total score												
 		(emotions_summary.reject {|emotion, score | emotion == 'sentiment' }).each do | emotion, score | 
+			puts "*" * 100
+			puts emotions_summary
+			puts "*" * 100
 			all_emotions_total += score
 		end
+
+		puts all_emotions_total
 
 		(emotions_summary.reject {|emotion, score | emotion == 'sentiment' }).each do | emotion, score | # next, change the scores to their percent of the total
 			emotions_summary[emotion] = ((score / all_emotions_total) * 100).round(2)
